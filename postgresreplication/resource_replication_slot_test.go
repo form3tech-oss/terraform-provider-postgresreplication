85924bfcae16d8c1cabd5ac4da26a36458653d78
Sam <sam@ivoryowensconsulting.co.uk>
2019-11-20 15:22:35 +0000

    review changes

--- postgresreplication/resource_replication_slot_test.go
+++ postgresreplication/resource_replication_slot_test.go
@@ -12,13 +12,11 @@ import (
 
 const (
 	testAccSlotCreate = `
-
-resource "postgresreplication_slot" "test_slot" {
-    slot_name 	   = "test_slot"
-    output_plugin  = "wal2json"
-	database       = "postgres"
-}
-
+		resource "postgresreplication_slot" "test_slot" {
+			slot_name 	   = "test_slot"
+			output_plugin  = "wal2json"
+			database       = "postgres"
+		}
 `
 )
 
